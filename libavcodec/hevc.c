fca39b1300d320b020dc59f4ea3ba6873491a1a3
Mickaël Raulet <mraulet@insa-rennes.fr>
2013-05-31 01:32:11 +0200

    hevc: start to support I frame with multiple slices

--- libavcodec/hevc.c
+++ libavcodec/hevc.c
@@ -1450,9 +1450,11 @@ static int luma_intra_pred_mode(HEVCContext *s, int x0, int y0, int pu_size,
     int y_pu = y0 >> s->sps->log2_min_pu_size;
     int pic_width_in_min_pu = s->sps->pic_width_in_min_cbs << 2;
     int size_in_pus = pu_size >> s->sps->log2_min_pu_size;
+    int x0b = x0 & ((1 << s->sps->log2_ctb_size) - 1);
+    int y0b = y0 & ((1 << s->sps->log2_ctb_size) - 1);
 
-    int cand_up   = y_pu > (s->ytiles_0 >> s->sps->log2_min_pu_size) ? s->pu.top_ipm[x_pu] : INTRA_DC ;
-    int cand_left = x_pu > (s->xtiles_0 >> s->sps->log2_min_pu_size) ? s->pu.left_ipm[y_pu] : INTRA_DC ;
+    int cand_up   = (s->ctb_up_flag || y0b) ? s->pu.top_ipm[x_pu] : INTRA_DC ;
+    int cand_left = (s->ctb_left_flag || x0b) ? s->pu.left_ipm[y_pu] : INTRA_DC ;
 
     int y_ctb = (y0 >> (s->sps->log2_ctb_size)) << (s->sps->log2_ctb_size);
     MvField *tab_mvf = s->ref->tab_mvf;
@@ -1810,8 +1812,9 @@ static int hls_decode_entry(AVCodecContext *avctxt, void *isFilterThread)
             y_ctb = (s->ctb_addr_rs / ((s->sps->pic_width_in_luma_samples + (ctb_size - 1))/ctb_size)) * ctb_size;
             s->ctb_left_flag = ((x_ctb > s->xtiles_0) && (s->ctb_addr_in_slice > 0) &&
                                 (s->pps->tile_id[s->ctb_addr_ts] == s->pps->tile_id[s->ctb_addr_ts-1]));
-            s->ctb_up_flag = ((y_ctb > s->ytiles_0)  && (s->pps->tile_id[s->ctb_addr_ts] == s->pps->tile_id[s->pps->ctb_addr_rs_to_ts[s->ctb_addr_rs - s->sps->pic_width_in_ctbs]]));
-            printf("left %d up %d, x_ctb %d y_ctb %d\n",s->ctb_left_flag, s->ctb_up_flag, x_ctb, y_ctb);
+            s->ctb_up_flag = ((y_ctb > s->ytiles_0)  && (s->ctb_addr_in_slice >= s->sps->pic_width_in_ctbs) &&
+                              (s->pps->tile_id[s->ctb_addr_ts] == s->pps->tile_id[s->pps->ctb_addr_rs_to_ts[s->ctb_addr_rs - s->sps->pic_width_in_ctbs]]));
+//            printf("left %d up %d, x_ctb %d y_ctb %d\n",s->ctb_left_flag, s->ctb_up_flag, x_ctb, y_ctb);
             if (s->sh.slice_sample_adaptive_offset_flag[0] || s->sh.slice_sample_adaptive_offset_flag[1])
                 hls_sao_param(s, x_ctb >> s->sps->log2_ctb_size, y_ctb >> s->sps->log2_ctb_size, (s->ctb_addr_in_slice > 0), 0);
             more_data = hls_coding_quadtree(s, x_ctb, y_ctb, s->sps->log2_ctb_size, 0, 0);
