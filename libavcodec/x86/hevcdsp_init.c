d9f3e841295c2f6308f94bb7dd38312798b7b074
Mickaël Raulet <mraulet@insa-rennes.fr>
2013-07-04 15:54:29 +0200

    hevc: enable deblocking for chrominance

--- libavcodec/x86/hevcdsp_init.c
+++ libavcodec/x86/hevcdsp_init.c
@@ -66,8 +66,8 @@ void ff_hevcdsp_init_x86(HEVCDSPContext *c, const int bit_depth, const int pcm_d
 
                 if (EXTERNAL_SSE2(mm_flags)) {
                     if (!pcm_deblock) {
-//                        c->hevc_v_loop_filter_chroma = ff_hevc_v_loop_filter_chroma_8_sse2;
-//                        c->hevc_h_loop_filter_chroma = ff_hevc_h_loop_filter_chroma_8_sse2;
+                        c->hevc_v_loop_filter_chroma = ff_hevc_v_loop_filter_chroma_8_sse2;
+                        c->hevc_h_loop_filter_chroma = ff_hevc_h_loop_filter_chroma_8_sse2;
                     }
                 }
                 if (EXTERNAL_SSSE3(mm_flags)) {
