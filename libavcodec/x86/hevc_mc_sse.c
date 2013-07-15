e992aae55a1253093c9ffbd09f265c392965a420
plepere <pierre-edouard.lepere@insa-rennes.fr>
2013-07-15 15:22:35 +0200

    bugfix in unweighted prediction

--- libavcodec/x86/hevc_mc_sse.c
+++ libavcodec/x86/hevc_mc_sse.c
@@ -13,6 +13,61 @@
 
 #define BIT_DEPTH 8
 
+void ff_hevc_put_unweighted_pred_8_sse(uint8_t *_dst, ptrdiff_t dststride,
+        int16_t *src, ptrdiff_t srcstride, int width, int height) {
+    int x, y;
+    uint8_t *dst = (uint8_t*) _dst;
+    __m128i r0, r1, f0;
+
+    f0 = _mm_set1_epi16(32);
+
+    if(width ==4)
+    for (y = 0; y < height; y++) {
+
+            r0 = _mm_load_si128((__m128i *) src);
+            r0 = _mm_adds_epi16(r0, f0);
+
+            r0 = _mm_srai_epi16(r0, 6);
+            r0 = _mm_packus_epi16(r0, r0);
+           _mm_maskmoveu_si128(r0,_mm_set_epi8(0,0,0,0,0,0,0,0,0,0,0,0,-1,-1,-1,-1),(char *) dst);
+           // _mm_storeu_si128((__m128i *) dst, r0);
+
+        dst += dststride;
+        src += srcstride;
+    }
+    else if (width ==8){
+        for (y = 0; y < height; y++) {
+                r0 = _mm_load_si128((__m128i *) src);
+
+                r0 = _mm_adds_epi16(r0, f0);
+
+                r0 = _mm_srai_epi16(r0, 6);
+                r0 = _mm_packus_epi16(r0, r1);
+
+                _mm_storel_epi64((__m128i *) dst, r0);
+
+            dst += dststride;
+            src += srcstride;
+        }
+    }else
+        for (y = 0; y < height; y++) {
+            for (x = 0; x < width; x += 16) {
+                r0 = _mm_load_si128((__m128i *) &src[x]);
+
+                r1 = _mm_load_si128((__m128i *) &src[x + 8]);
+                r0 = _mm_adds_epi16(r0, f0);
+
+                r1 = _mm_adds_epi16(r1, f0);
+                r0 = _mm_srai_epi16(r0, 6);
+                r1 = _mm_srai_epi16(r1, 6);
+                r0 = _mm_packus_epi16(r0, r1);
+
+                _mm_storeu_si128((__m128i *) &dst[x], r0);
+            }
+            dst += dststride;
+            src += srcstride;
+        }
+}
 
 void ff_hevc_put_unweighted_pred_sse(uint8_t *_dst, ptrdiff_t _dststride,
         int16_t *src, ptrdiff_t srcstride, int width, int height) {
