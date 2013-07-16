738bd782598b684eb11ded7b49f2cdd5dcdb3818
Wassim Hamidouche <Wassim.Hamidouche@insa-rennes.fr>
2013-07-16 14:35:54 +0200

    desable not working SSE function

--- libavcodec/x86/hevcdsp_init.c
+++ libavcodec/x86/hevcdsp_init.c
@@ -144,10 +144,10 @@ void ff_hevcdsp_init_x86(HEVCDSPContext *c, const int bit_depth, const int pcm_d
                 if (EXTERNAL_SSE4(mm_flags)) {
 #if ARCH_X86_64
 #if GCC_VERSION > MIN_GCC_VERSION_MC || __APPLE__
-                	c->put_unweighted_pred = ff_hevc_put_unweighted_pred_8_sse;
-                	c->put_weighted_pred_avg = ff_hevc_put_weighted_pred_avg_8_sse;
-                	c->weighted_pred = ff_hevc_weighted_pred_8_sse;
-                	c->weighted_pred_avg = ff_hevc_weighted_pred_avg_8_sse;
+               // 	c->put_unweighted_pred = ff_hevc_put_unweighted_pred_8_sse;
+               // 	c->put_weighted_pred_avg = ff_hevc_put_weighted_pred_avg_8_sse;
+               // 	c->weighted_pred = ff_hevc_weighted_pred_8_sse;
+               // 	c->weighted_pred_avg = ff_hevc_weighted_pred_avg_8_sse;
 
 
                 	c->put_hevc_epel[0][0] = ff_hevc_put_hevc_epel_pixels_8_sse;
