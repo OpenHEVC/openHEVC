7fd9f784510c6f841cae2a4aa2a4b9ee10aabd08
Mickaël Raulet <mraulet@insa-rennes.fr>
2013-11-18 22:07:05 +0100

    fix asm call

--- libavcodec/x86/hevcdsp_init.c
+++ libavcodec/x86/hevcdsp_init.c
@@ -29,7 +29,7 @@
 
 /***********************************/
 /* MC */
-void ff_put_hevc_mc_pixels_8_sse4(int16_t *dst, ptrdiff_t dststride, uint8_t *_src, ptrdiff_t _srcstride, int width, int height, int mx, int my, int16_t* mcbuffer);
+void ff_put_hevc_mc_pixels_2_8_sse4(int16_t *dst, ptrdiff_t dststride, uint8_t *_src, ptrdiff_t _srcstride, int width, int height, int mx, int my, int16_t* mcbuffer);
 
 /***********************************/
 /* deblocking */
@@ -85,7 +85,7 @@ void ff_hevcdsp_init_x86(HEVCDSPContext *c, const int bit_depth)
 
                     c->put_unweighted_pred = ff_hevc_put_unweighted_pred_8_sse;
 
-                    c->put_hevc_qpel[0][0] = ff_put_hevc_mc_pixels_8_sse4;
+                    c->put_hevc_qpel[0][0] = ff_hevc_put_hevc_qpel_pixels_8_sse;
                     c->put_hevc_qpel[0][1] = ff_hevc_put_hevc_qpel_h_1_8_sse;
                     c->put_hevc_qpel[0][2] = ff_hevc_put_hevc_qpel_h_2_8_sse;
                     c->put_hevc_qpel[0][3] = ff_hevc_put_hevc_qpel_h_3_8_sse;
@@ -107,7 +107,7 @@ void ff_hevcdsp_init_x86(HEVCDSPContext *c, const int bit_depth)
                 	c->weighted_pred_avg = ff_hevc_weighted_pred_avg_8_sse;
 
                 	for(i = 0; i<=3;i++){
-                	c->put_hevc_epel[0][0][i] = ff_put_hevc_mc_pixels_8_sse4;
+                	c->put_hevc_epel[0][0][i] = ff_put_hevc_mc_pixels_2_8_sse4;
                 	c->put_hevc_epel[0][1][i] = ff_hevc_put_hevc_epel_h_8_sse;
                 	c->put_hevc_epel[1][0][i] = ff_hevc_put_hevc_epel_v_8_sse;
                 	c->put_hevc_epel[1][1][i] = ff_hevc_put_hevc_epel_hv_8_sse;
