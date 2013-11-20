616cce1966ddafa6c11d95f1e517fbcd4b7089c0
Mickaël Raulet <mraulet@insa-rennes.fr>
2013-11-20 11:19:52 +0100

    add lpgl license to the wrapper

--- gpac/modules/openhevc_dec/openHevcWrapper.c
+++ gpac/modules/openhevc_dec/openHevcWrapper.c
@@ -1,3 +1,23 @@
+/*
+ * openhevc.c wrapper to openhevc or ffmpeg
+ * Copyright (c) 2012-2013 Micka‘l Raulet, Wassim Hamidouche, Gildas Cocherel, Pierre Edouard Lepere
+ *
+ * This file is part of openhevc.
+ *
+ * openHevc is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * openhevc is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with openhevc; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
 #include <stdio.h>
 #include "openHevcWrapper.h"
 #include "libavcodec/avcodec.h"
@@ -21,6 +41,7 @@ typedef struct OpenHevcWrapperContexts {
     OpenHevcWrapperContext **wraper;
     int nb_decoders;
     int active_layer;
+    int set_vps;
 } OpenHevcWrapperContexts;
 
 OpenHevc_Handle libOpenHevcInit(int nb_pthreads, int thread_type)
--- gpac/modules/openhevc_dec/openHevcWrapper.h
+++ gpac/modules/openhevc_dec/openHevcWrapper.h
@@ -1,3 +1,24 @@
+/*
+ * openhevc.h wrapper to openhevc or ffmpeg
+ * Copyright (c) 2012-2013 Micka‘l Raulet, Wassim Hamidouche, Gildas Cocherel, Pierre Edouard Lepere
+ *
+ * This file is part of openhevc.
+ *
+ * openHevc is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * openhevc is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with openhevc; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
 #ifndef OPEN_HEVC_WRAPPER_H
 #define OPEN_HEVC_WRAPPER_H
 
