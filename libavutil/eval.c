3bb209b2dfe0707dd5c492e1c2e6ec779f7f5190
kjerbi <khaled_jerbi@yahoo.fr>
2014-03-06 14:51:31 +0100

    add avpriv_snprintf directly to file since include is not possible (to be cleaned later)

--- libavutil/eval.c
+++ libavutil/eval.c
@@ -73,7 +73,81 @@ static const int8_t si_prefixes['z' - 'E' + 1] = {
     ['Z'-'E']=  21,
     ['Y'-'E']=  24,
 };
+static char *check_nan_suffix(char *s)
+{
+	char *start = s;
 
+	if (*s++ != '(')
+		return start;
+
+	while ((*s >= 'a' && *s <= 'z') || (*s >= 'A' && *s <= 'Z') ||
+		(*s >= '0' && *s <= '9') || *s == '_')
+		s++;
+
+	return *s == ')' ? s + 1 : start;
+}
+
+#undef strtod
+double strtod(const char *, char **);
+
+double avpriv_strtod(const char *nptr, char **endptr)
+{
+	char *end;
+	double res;
+
+	/* Skip leading spaces */
+	while (av_isspace(*nptr))
+		nptr++;
+
+	if (!av_strncasecmp(nptr, "infinity", 8)) {
+		end = nptr + 8;
+		res = INFINITY;
+	}
+	else if (!av_strncasecmp(nptr, "inf", 3)) {
+		end = nptr + 3;
+		res = INFINITY;
+	}
+	else if (!av_strncasecmp(nptr, "+infinity", 9)) {
+		end = nptr + 9;
+		res = INFINITY;
+	}
+	else if (!av_strncasecmp(nptr, "+inf", 4)) {
+		end = nptr + 4;
+		res = INFINITY;
+	}
+	else if (!av_strncasecmp(nptr, "-infinity", 9)) {
+		end = nptr + 9;
+		res = -INFINITY;
+	}
+	else if (!av_strncasecmp(nptr, "-inf", 4)) {
+		end = nptr + 4;
+		res = -INFINITY;
+	}
+	else if (!av_strncasecmp(nptr, "nan", 3)) {
+		end = check_nan_suffix(nptr + 3);
+		res = NAN;
+	}
+	else if (!av_strncasecmp(nptr, "+nan", 4) ||
+		!av_strncasecmp(nptr, "-nan", 4)) {
+		end = check_nan_suffix(nptr + 4);
+		res = NAN;
+	}
+	else if (!av_strncasecmp(nptr, "0x", 2) ||
+		!av_strncasecmp(nptr, "-0x", 3) ||
+		!av_strncasecmp(nptr, "+0x", 3)) {
+		/* FIXME this doesn't handle exponents, non-integers (float/double)
+		* and numbers too large for long long */
+		res = strtoll(nptr, &end, 16);
+	}
+	else {
+		res = strtod(nptr, &end);
+	}
+
+	if (endptr)
+		*endptr = end;
+
+	return res;
+}
 double av_strtod(const char *numstr, char **tail)
 {
     double d;
