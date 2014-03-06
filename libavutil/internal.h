37170e2be4a0181537cb693ec45a451571bfbab8
kjerbi <khaled_jerbi@yahoo.fr>
2014-03-06 14:50:48 +0100

    remove forcing external prefix for snprintf and strtod for correct compilation on 32bits

--- libavutil/internal.h
+++ libavutil/internal.h
@@ -120,8 +120,8 @@
 #include "libm.h"
 
 #if defined(_MSC_VER) && !CONFIG_SHARED
-#pragma comment(linker, "/include:"EXTERN_PREFIX"avpriv_strtod")
-#pragma comment(linker, "/include:"EXTERN_PREFIX"avpriv_snprintf")
+//#pragma comment(linker, "/include:"EXTERN_PREFIX"avpriv_strtod")
+//#pragma comment(linker, "/include:"EXTERN_PREFIX"avpriv_snprintf")
 #endif
 
 /**
