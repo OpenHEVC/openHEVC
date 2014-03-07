db5cb4d951c3eb2e1c61b0b4429c315d1994a483
kjerbi <khaled_jerbi@yahoo.fr>
2014-03-07 11:52:01 +0100

    active SSE4 functions

--- libavcodec/x86/hevcdsp_init.c
+++ libavcodec/x86/hevcdsp_init.c
@@ -80,12 +80,12 @@ void ff_hevcdsp_init_x86(HEVCDSPContext *c, const int bit_depth)
 #endif /* ARCH_X86_32 && HAVE_MMXEXT_EXTERNAL */
 
                 if (EXTERNAL_SSE2(mm_flags)) {
-                   /* c->hevc_v_loop_filter_chroma = ff_hevc_v_loop_filter_chroma_8_sse2;
-                    c->hevc_h_loop_filter_chroma = ff_hevc_h_loop_filter_chroma_8_sse2;*/
+                    c->hevc_v_loop_filter_chroma = ff_hevc_v_loop_filter_chroma_8_sse2;
+                    c->hevc_h_loop_filter_chroma = ff_hevc_h_loop_filter_chroma_8_sse2;
                 }
                 if (EXTERNAL_SSSE3(mm_flags)) {
 
-                   /* c->transform_4x4_luma_add = ff_hevc_transform_4x4_luma_add_8_sse4;
+                    c->transform_4x4_luma_add = ff_hevc_transform_4x4_luma_add_8_sse4;
                     c->transform_add[0] = ff_hevc_transform_4x4_add_8_sse4;
                     c->transform_add[1] = ff_hevc_transform_8x8_add_8_sse4;
                     c->transform_add[2] = ff_hevc_transform_16x16_add_8_sse4;
@@ -114,11 +114,11 @@ void ff_hevcdsp_init_x86(HEVCDSPContext *c, const int bit_depth)
                     PEL_LINK_SSE(c->put_hevc_qpel, 1, 1, 1, qpel_hv8,  8);
                     PEL_LINK_SSE(c->put_hevc_qpel, 2, 1, 1, qpel_hv8,  8);
                     PEL_LINK_SSE(c->put_hevc_qpel, 3, 1, 1, qpel_hv8,  8);
-                    PEL_LINK_SSE(c->put_hevc_qpel, 4, 1, 1, qpel_hv8,  8);*/
+                    PEL_LINK_SSE(c->put_hevc_qpel, 4, 1, 1, qpel_hv8,  8);
 
 #if ARCH_X86_64
-                   /* c->hevc_v_loop_filter_luma = ff_hevc_v_loop_filter_luma_8_ssse3;
-                    c->hevc_h_loop_filter_luma = ff_hevc_h_loop_filter_luma_8_ssse3;*/
+                    c->hevc_v_loop_filter_luma = ff_hevc_v_loop_filter_luma_8_ssse3;
+                    c->hevc_h_loop_filter_luma = ff_hevc_h_loop_filter_luma_8_ssse3;
 #endif
                 }
 #ifdef __SSE4_1__
