bfde30294615eaaedbe024f6b6fd208c5bf7be1e
Morgan LACOUR <mlacour@insa-rennes.fr>
2015-04-17 10:12:05 +0200

    Add Luma 1 & 3 Taps Add eco_param.h

--- libavcodec/eco_param.h
+++ libavcodec/eco_param.h
@@ -5,7 +5,7 @@
 
 //#define ECO_DBF_OFF	// disable deblocking filter
 
-#define ECO_LUMA3	// number of taps for luma filters [1;3;7]
+#define ECO_LUMA1	// number of taps for luma filters [1;3;7]
 
 //#define ECO_CHROMA	// number of taps for chroma filters [1;4]
 
