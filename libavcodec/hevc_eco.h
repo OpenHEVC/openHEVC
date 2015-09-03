66ac4a8ba27a7fcf072821d748939a788145ec70
Morgan LACOUR <morgan.lacour@gmail.com>
2015-09-03 14:05:17 +0200

    includes and rm useless file

--- libavcodec/hevc_eco.h
+++ libavcodec/hevc_eco.h
@@ -3,6 +3,9 @@
  * Morgan Lacour 2015
  */
 
+#ifndef HEVC_ECO_H
+#define HEVC_ECO_H
+
 #include "hevc.h"
 
 void eco_get_activation(HEVCContext *s);
@@ -11,3 +14,5 @@ void eco_set_activation(HEVCContext *s);
 void eco_logs(HEVCContext *s);
 
 void eco_update_context(HEVCContext *s, HEVCContext *s0);
+
+#endif
