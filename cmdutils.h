b93ab44204eee910fd60c39f04841b3260c8ce35
Pierre-Loup Cabarat <pcabarat@insa-rennes.fr>
2017-04-14 15:49:20 +0200

    Remove avfilter.h and swscale.h inclusion in cmdutils.h

--- cmdutils.h
+++ cmdutils.h
@@ -26,9 +26,7 @@
 
 #include "config.h"
 #include "libavcodec/avcodec.h"
-#include "libavfilter/avfilter.h"
 #include "libavformat/avformat.h"
-#include "libswscale/swscale.h"
 
 #ifdef _WIN32
 #undef main /* We don't want SDL to override our main() */
