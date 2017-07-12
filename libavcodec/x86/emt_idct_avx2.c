1ab4ce07e8247fd1ff949f9cbc144ef455f8b82a
Pierre-Loup Cabarat <pcabarat@insa-renne.fr>
2017-07-12 13:19:59 +0200

    (tmp) Comment out unrequired operation + renaming of DCT coefficients vectors

--- libavcodec/x86/emt_idct_avx2.c
+++ libavcodec/x86/emt_idct_avx2.c
@@ -8,6 +8,8 @@
 #include <emmintrin.h>
 #endif
 
+#include <time.h>
+#include "syscall.h"
 
 #include <smmintrin.h>
 
@@ -20,43 +22,54 @@
 #endif
 
 
+//extern __inline__ uint64_t rdtsc(void) {
+//  uint64_t x;
+//  x = clock_gettime(CLOCK_MONOTONIC)
+//  //__asm__ volatile ("rdtsc" : "=A" (x));
+//  return x;
+//}
+
+
 void FUNC(emt_idst_VII_4x4_v_avx2) (int16_t *x, int16_t *block, int log2_transform_range, const int clip_min, const int clip_max)
 {
-    const __m128i zeros = _mm_setzero_si128();
-    const __m128i add  = _mm_set1_epi32(ADD_EMT_V);
+    //const __m128i zeros = _mm_setzero_si128();
+    //const __m128i add  = _mm_set1_epi32(ADD_EMT_V);
 
-    const __m128i max  = _mm_set1_epi32(clip_max);
-    const __m128i min  = _mm_set1_epi32(clip_min);
+    //const __m128i max  = _mm_set1_epi16(clip_max);
+    //const __m128i min  = _mm_set1_epi16(clip_min);
 
-    const __m128i a = _mm_set1_epi16(-336);
-    const __m128i b = _mm_set1_epi16(296);
-    const __m128i c = _mm_set1_epi16(219);
-    const __m128i d = _mm_set1_epi16(117);
+//    const __m128i a_VII = _mm_set1_epi16(-336);
+//    const __m128i b_VII = _mm_set1_epi16(296);
+//    const __m128i c_VII = _mm_set1_epi16(219);
+//    const __m128i d_VII = _mm_set1_epi16(117);
+//     struct timespec t1,t2;
 
+//     clock_gettime(CLOCK_REALTIME,&t1);
     __m128i c0, c1,c2,c3,c4, x0, x8, x12, x4;
 
       x0  = _mm_load_si128((__m128i *)x);
-      x8  = _mm_load_si128((__m128i *)&x[8]);
-      x12 = _mm_loadu_si128((__m128i *)&x[12]);
-      x4  = _mm_loadu_si128((__m128i *)&x[4]);
+      x8  = _mm_loadl_epi64((__m128i *)&x[8]);
+      x12 = _mm_loadl_epi64((__m128i *)&x[12]);
+      //x4  = _mm_loadl_epi64((__m128i *)&x[4]);
+      //x12 = _mm_shuffle_epi32(x8,0b01001110);
 
       c0 = _mm_unpacklo_epi16(x8,x12);
       c2 = _mm_unpacklo_epi16(x0,x8);
 
-      c3 = _mm_sub_epi16(zeros,x12);
-      c3 = _mm_unpacklo_epi16(x0,c3);
+      //c3 = _mm_sub_epi16(_mm_setzero_si128(),x12);
+      c3 = _mm_unpacklo_epi16(x0,x12);
 
-      c4 = _mm_unpacklo_epi16(x4,zeros);
+      c4 = _mm_unpackhi_epi16(x0,_mm_setzero_si128());
 
-      c1 = _mm_subs_epi16(x0,x8);
+      c1 = _mm_sub_epi16(x0,x8);
       c1 = _mm_unpacklo_epi16(c1,x12);
 
-      c0 = _mm_madd_epi16(c0,a);
-      c1 = _mm_madd_epi16(c1,b);
-      c2 = _mm_madd_epi16(c2,c);
-      c3 = _mm_madd_epi16(c3,d);
+      c0 = _mm_madd_epi16(c0,_mm_set1_epi16(-336));
+      c1 = _mm_madd_epi16(c1,_mm_set1_epi16(296));
+      c2 = _mm_madd_epi16(c2,_mm_set1_epi16(219));
+      c3 = _mm_madd_epi16(c3,_mm_set_epi16(-117,117,-117,117,-117,117,-117,117));
 
-      c4 = _mm_madd_epi16(c4,b);
+      c4 = _mm_madd_epi16(c4,_mm_set1_epi16(296));
 
       x0 = _mm_sub_epi32(c3,c0);
       x0 = _mm_add_epi32(x0,c4);
@@ -67,41 +80,50 @@ void FUNC(emt_idst_VII_4x4_v_avx2) (int16_t *x, int16_t *block, int log2_transfo
       x4 = _mm_add_epi32(c2,c0);
       x4 = _mm_add_epi32(x4,c4);
 
-      x0 =  _mm_add_epi32(x0,add);
-      x4 =  _mm_add_epi32(x4,add);
-      x8 =  _mm_add_epi32(c1,add);
-      x12 = _mm_add_epi32(x12,add);
+      x0 =  _mm_add_epi32(x0, _mm_set1_epi32(ADD_EMT_V));
+      x4 =  _mm_add_epi32(x4, _mm_set1_epi32(ADD_EMT_V));
+      x8 =  _mm_add_epi32(c1, _mm_set1_epi32(ADD_EMT_V));
+      x12 = _mm_add_epi32(x12,_mm_set1_epi32(ADD_EMT_V));
 
-      x0 =  _mm_srai_epi32(x0,SHIFT_EMT_V);
-      x4 =  _mm_srai_epi32(x4,SHIFT_EMT_V);
-      x8 =  _mm_srai_epi32(x8,SHIFT_EMT_V);
+      x0 =  _mm_srai_epi32(x0, SHIFT_EMT_V);
+      x4 =  _mm_srai_epi32(x4, SHIFT_EMT_V);
+      x8 =  _mm_srai_epi32(x8, SHIFT_EMT_V);
       x12 = _mm_srai_epi32(x12,SHIFT_EMT_V);
 
-      x0 = _mm_min_epi32(x0,max);
-      x0 = _mm_max_epi32(x0,min);
+//      c0 = _mm_unpacklo_epi32(x0,x8);
+//      c1 = _mm_unpacklo_epi32(x4,x12);
 
-      x4 = _mm_min_epi32(x4,max);
-      x4 = _mm_max_epi32(x4,min);
+//      c2 = _mm_unpackhi_epi32(x0,x8);
+//      c3 = _mm_unpackhi_epi32(x4,x12);
 
-      x8 = _mm_min_epi32(x8,max);
-      x8 = _mm_max_epi32(x8,min);
+//      c0 = _mm_packs_epi32(c0,c2);
+//      c1 = _mm_packs_epi32(c1,c3);
+      c0 = _mm_packs_epi32(x0,x8);
+      c1 = _mm_packs_epi32(x4,x12);
 
-      x12 = _mm_min_epi32(x12,max);
-      x12 = _mm_max_epi32(x12,min);
+      c0 = _mm_shuffle_epi32(c0,0b11011000);
+      c1 = _mm_shuffle_epi32(c1,0b11011000);
 
-      c0 = _mm_unpacklo_epi32(x0,x8);
-      c1 = _mm_unpacklo_epi32(x4,x12);
-      c2 = _mm_unpackhi_epi32(x0,x8);
-      c3 = _mm_unpackhi_epi32(x4,x12);
+// We should not need clipping here since a last clip is done in h funct and 16
+// we already used 16 bits saturation
+//      c0 = _mm_min_epi16(c0,max);
+//      c0 = _mm_max_epi16(c0,min);
 
-      c0 = _mm_packs_epi32(c0,c2);
-      c1 = _mm_packs_epi32(c1,c3);
+//      c1 = _mm_min_epi16(c1,max);
+//      c1 = _mm_max_epi16(c1,min);
 
       x0 = _mm_unpacklo_epi16(c0,c1);
       x4 = _mm_unpackhi_epi16(c0,c1);
 
+      x0 = _mm_shuffle_epi32(x0,0b11011000);
+      x4 = _mm_shuffle_epi32(x4,0b11011000);
+
       _mm_store_si128((__m128i *)&block[0],x0);
       _mm_store_si128((__m128i *)&block[8],x4);
+
+      //uint64_t t2 = rdtsc();
+//      clock_gettime(CLOCK_REALTIME,&t2);
+//      fprintf(stderr,"num cycles:%ld, %ld\n",t2.tv_nsec-t1.tv_nsec,t2.tv_sec-t1.tv_sec);
 }
 
 
@@ -112,38 +134,39 @@ void FUNC(emt_idst_VII_4x4_h_avx2)(int16_t *x, int16_t *block, int log2_transfor
   const __m128i zeros = _mm_setzero_si128();
   const __m128i add  = _mm_set1_epi32(1 << (shift - 1));
 
-  const  __m128i max  = _mm_set1_epi32(clip_max);
-  const __m128i min  = _mm_set1_epi32(clip_min);
+  const __m128i max  = _mm_set1_epi16(clip_max);
+  const __m128i min  = _mm_set1_epi16(clip_min);
 
-  const __m128i a = _mm_set1_epi16(-336);
-  const __m128i b = _mm_set1_epi16(296);
-  const __m128i c = _mm_set1_epi16(219);
-  const __m128i d = _mm_set1_epi16(117);
+  const __m128i a_VII = _mm_set1_epi16(-336);
+  const __m128i b_VII = _mm_set1_epi16(296);
+  const __m128i c_VII = _mm_set1_epi16(219);
+  const __m128i d_VII = _mm_set_epi16(-117,117,-117,117,-117,117,-117,117);
 
   __m128i c0, c1,c2,c3,c4, x0, x8, x12, x4;
 
     x0  = _mm_load_si128((__m128i *)x);
-    x8  = _mm_load_si128((__m128i *)&x[8]);
-    x12 = _mm_loadu_si128((__m128i *)&x[12]);
-    x4  = _mm_loadu_si128((__m128i *)&x[4]);
+    x8  = _mm_loadl_epi64((__m128i *)&x[8]);
+    x12 = _mm_loadl_epi64((__m128i *)&x[12]);
+    //x4  = _mm_loadl_epi64((__m128i *)&x[4]);
+    //x12 = _mm_shuffle_epi32(x8,0b01001110);
 
     c0 = _mm_unpacklo_epi16(x8,x12);
     c2 = _mm_unpacklo_epi16(x0,x8);
 
-    c3 = _mm_sub_epi16(zeros,x12);
-    c3 = _mm_unpacklo_epi16(x0,c3);
+    //c3 = _mm_sub_epi16(zeros,x12);
+    c3 = _mm_unpacklo_epi16(x0,x12);
 
-    c4 = _mm_unpacklo_epi16(x4,zeros);
+    c4 = _mm_unpackhi_epi16(x0,zeros);
 
-    c1 = _mm_subs_epi16(x0,x8);
+    c1 = _mm_sub_epi16(x0,x8);
     c1 = _mm_unpacklo_epi16(c1,x12);
 
-    c0 = _mm_madd_epi16(c0,a);
-    c1 = _mm_madd_epi16(c1,b);
-    c2 = _mm_madd_epi16(c2,c);
-    c3 = _mm_madd_epi16(c3,d);
+    c0 = _mm_madd_epi16(c0,a_VII);
+    c1 = _mm_madd_epi16(c1,b_VII);
+    c2 = _mm_madd_epi16(c2,c_VII);
+    c3 = _mm_madd_epi16(c3,d_VII);
 
-    c4 = _mm_madd_epi16(c4,b);
+    c4 = _mm_madd_epi16(c4,b_VII);
 
     x0 = _mm_sub_epi32(c3,c0);
     x0 = _mm_add_epi32(x0,c4);
@@ -164,18 +187,6 @@ void FUNC(emt_idst_VII_4x4_h_avx2)(int16_t *x, int16_t *block, int log2_transfor
     x8 =  _mm_srai_epi32(x8,shift);
     x12 = _mm_srai_epi32(x12,shift);
 
-    x0 = _mm_min_epi32(x0,max);
-    x0 = _mm_max_epi32(x0,min);
-
-    x4 = _mm_min_epi32(x4,max);
-    x4 = _mm_max_epi32(x4,min);
-
-    x8 = _mm_min_epi32(x8,max);
-    x8 = _mm_max_epi32(x8,min);
-
-    x12 = _mm_min_epi32(x12,max);
-    x12 = _mm_max_epi32(x12,min);
-
     c0 = _mm_unpacklo_epi32(x0,x8);
     c1 = _mm_unpacklo_epi32(x4,x12);
     c2 = _mm_unpackhi_epi32(x0,x8);
@@ -183,6 +194,12 @@ void FUNC(emt_idst_VII_4x4_h_avx2)(int16_t *x, int16_t *block, int log2_transfor
 
     c0 = _mm_packs_epi32(c0,c2);
     c1 = _mm_packs_epi32(c1,c3);
+
+    c0 = _mm_min_epi16(c0,max);
+    c0 = _mm_max_epi16(c0,min);
+
+    c1 = _mm_min_epi16(c1,max);
+    c1 = _mm_max_epi16(c1,min);
 
     x0 = _mm_unpacklo_epi16(c0,c1);
     x4 = _mm_unpackhi_epi16(c0,c1);
@@ -196,41 +213,40 @@ void FUNC(emt_idct_VIII_4x4_v_avx2) (int16_t *x, int16_t *block, int log2_transf
     const __m128i zeros = _mm_setzero_si128();
     const __m128i add  = _mm_set1_epi32(ADD_EMT_V);
 
-    const __m128i max  = _mm_set1_epi32(clip_max);
-    const __m128i min  = _mm_set1_epi32(clip_min);
+    const __m128i max  = _mm_set1_epi16(clip_max);
+    const __m128i min  = _mm_set1_epi16(clip_min);
 
-    const __m128i a = _mm_set1_epi16(219);
-    const __m128i b = _mm_set1_epi16(296);
-    const __m128i c = _mm_set1_epi16(117);
-    const __m128i d = _mm_set1_epi16(336);
+    const __m128i a_VIII = _mm_set_epi16(-219,219,-219,219,-219,219,-219,219);
+    const __m128i b_VIII = _mm_set_epi16(-296,296,-296,296,-296,296,-296,296);
+    const __m128i c_VIII = _mm_set1_epi16(117);
+    const __m128i d_VIII = _mm_set1_epi16(336);
 
     __m128i c0, c1,c2,c3,c4, x0, x8, x12, x4;
 
     x0  = _mm_load_si128((__m128i *)x);
-    x8  = _mm_load_si128((__m128i *)&x[8]);
-    x12 = _mm_loadu_si128((__m128i *)&x[12]);
-    x4  = _mm_loadu_si128((__m128i *)&x[4]);
+    x8  = _mm_loadl_epi64((__m128i *)&x[8]);
+    x12 = _mm_loadl_epi64((__m128i *)&x[12]);
+    //x4  = _mm_loadl_epi64((__m128i *)&x[4]);
+    //x12 = _mm_shuffle_epi32(x8,0b01001110);
 
+    //FIXME: risk of integer overflow?
+    c1 = _mm_sub_epi16(x0,x8);
 
-    c1 = _mm_subs_epi16(x0,x8);
+    //c0 used to store the opposit of x12 used 2x
+    //c0 = _mm_sub_epi16(zeros,x12);
 
-    c0 = _mm_sub_epi16(zeros,x12);
-    c1 = _mm_unpacklo_epi16(c1,c0);
-    c0 = _mm_unpacklo_epi16(x8,c0);
-
+    c1 = _mm_unpacklo_epi16(c1,x12);
+    c0 = _mm_unpacklo_epi16(x8,x12);
     c2 = _mm_unpacklo_epi16(x0,x8);
-
     c3 = _mm_unpacklo_epi16(x0,x12);
 
-    c4 = _mm_unpacklo_epi16(x4,zeros);
+    c4 = _mm_unpackhi_epi16(x0,zeros);
 
-
-    c0 = _mm_madd_epi16(c0,a);
-    c1 = _mm_madd_epi16(c1,b);
-    c2 = _mm_madd_epi16(c2,c);
-    c3 = _mm_madd_epi16(c3,d);
-
-    c4 = _mm_madd_epi16(c4,b);
+    c0 = _mm_madd_epi16(c0,a_VIII);
+    c1 = _mm_madd_epi16(c1,b_VIII);
+    c2 = _mm_madd_epi16(c2,c_VIII);
+    c3 = _mm_madd_epi16(c3,d_VIII);
+    c4 = _mm_madd_epi16(c4,b_VIII);
 
     x0 = _mm_add_epi32(c3,c0);
     x0 = _mm_add_epi32(x0,c4);
@@ -251,25 +267,21 @@ void FUNC(emt_idct_VIII_4x4_v_avx2) (int16_t *x, int16_t *block, int log2_transf
     x8 =  _mm_srai_epi32(x8,SHIFT_EMT_V);
     x12 = _mm_srai_epi32(x12,SHIFT_EMT_V);
 
-    x0 = _mm_min_epi32(x0,max);
-    x0 = _mm_max_epi32(x0,min);
-
-    x4 = _mm_min_epi32(x4,max);
-    x4 = _mm_max_epi32(x4,min);
-
-    x8 = _mm_min_epi32(x8,max);
-    x8 = _mm_max_epi32(x8,min);
-
-    x12 = _mm_min_epi32(x12,max);
-    x12 = _mm_max_epi32(x12,min);
-
     c0 = _mm_unpacklo_epi32(x0,x8);
     c1 = _mm_unpacklo_epi32(x4,x12);
+
     c2 = _mm_unpackhi_epi32(x0,x8);
     c3 = _mm_unpackhi_epi32(x4,x12);
 
     c0 = _mm_packs_epi32(c0,c2);
     c1 = _mm_packs_epi32(c1,c3);
+
+// We should not need to clip here since a last clip is done in h funct
+//    c0 = _mm_min_epi16(c0,max);
+//    c0 = _mm_max_epi16(c0,min);
+
+//    c1 = _mm_min_epi16(c1,max);
+//    c1 = _mm_max_epi16(c1,min);
 
     x0 = _mm_unpacklo_epi16(c0,c1);
     x4 = _mm_unpackhi_epi16(c0,c1);
@@ -286,40 +298,42 @@ void FUNC(emt_idct_VIII_4x4_h_avx2)(int16_t *x, int16_t *block, int log2_transfo
   const __m128i zeros = _mm_setzero_si128();
   const __m128i add  = _mm_set1_epi32(1 << (shift - 1));
 
-  const __m128i max  = _mm_set1_epi32(clip_max);
-  const __m128i min  = _mm_set1_epi32(clip_min);
+  const __m128i max  = _mm_set1_epi16(clip_max);
+  const __m128i min  = _mm_set1_epi16(clip_min);
 
-  const __m128i a = _mm_set1_epi16(219);
-  const __m128i b = _mm_set1_epi16(296);
-  const __m128i c = _mm_set1_epi16(117);
-  const __m128i d = _mm_set1_epi16(336);
+  const __m128i a_VIII = _mm_set_epi16(-219,219,-219,219,-219,219,-219,219);
+  const __m128i b_VIII = _mm_set_epi16(-296,296,-296,296,-296,296,-296,296);
+  const __m128i c_VIII = _mm_set1_epi16(117);
+  const __m128i d_VIII = _mm_set1_epi16(336);
 
   __m128i c0, c1,c2,c3,c4, x0, x8, x12, x4;
 
     x0  = _mm_load_si128((__m128i *)x);
-    x8  = _mm_load_si128((__m128i *)&x[8]);
-    x12 = _mm_loadu_si128((__m128i *)&x[12]);
-    x4  = _mm_loadu_si128((__m128i *)&x[4]);
+    x8  = _mm_loadl_epi64((__m128i *)&x[8]);
+    x12 = _mm_loadl_epi64((__m128i *)&x[12]);
+    //x4  = _mm_loadl_epi64((__m128i *)&x[4]);
+    //x12 = _mm_shuffle_epi32(x8,0b01001110);
 
-    c1 = _mm_subs_epi16(x0,x8);
+    //FIXME: might overflow?
+    c1 = _mm_sub_epi16(x0,x8);
 
-    c0 = _mm_sub_epi16(zeros,x12);
-    c1 = _mm_unpacklo_epi16(c1,c0);
-    c0 = _mm_unpacklo_epi16(x8,c0);
+    //c0 = _mm_sub_epi16(zeros,x12);
+
+    c1 = _mm_unpacklo_epi16(c1,x12);
+    c0 = _mm_unpacklo_epi16(x8,x12);
 
     c2 = _mm_unpacklo_epi16(x0,x8);
 
     c3 = _mm_unpacklo_epi16(x0,x12);
 
-    c4 = _mm_unpacklo_epi16(x4,zeros);
+    c4 = _mm_unpackhi_epi16(x0,zeros);
 
+    c0 = _mm_madd_epi16(c0,a_VIII);
+    c1 = _mm_madd_epi16(c1,b_VIII);
+    c2 = _mm_madd_epi16(c2,c_VIII);
+    c3 = _mm_madd_epi16(c3,d_VIII);
 
-    c0 = _mm_madd_epi16(c0,a);
-    c1 = _mm_madd_epi16(c1,b);
-    c2 = _mm_madd_epi16(c2,c);
-    c3 = _mm_madd_epi16(c3,d);
-
-    c4 = _mm_madd_epi16(c4,b);
+    c4 = _mm_madd_epi16(c4,b_VIII);
 
     x0 = _mm_add_epi32(c3,c0);
     x0 = _mm_add_epi32(x0,c4);
@@ -340,18 +354,6 @@ void FUNC(emt_idct_VIII_4x4_h_avx2)(int16_t *x, int16_t *block, int log2_transfo
     x8 =  _mm_srai_epi32(x8,shift);
     x12 = _mm_srai_epi32(x12,shift);
 
-    x0 = _mm_min_epi32(x0,max);
-    x0 = _mm_max_epi32(x0,min);
-
-    x4 = _mm_min_epi32(x4,max);
-    x4 = _mm_max_epi32(x4,min);
-
-    x8 = _mm_min_epi32(x8,max);
-    x8 = _mm_max_epi32(x8,min);
-
-    x12 = _mm_min_epi32(x12,max);
-    x12 = _mm_max_epi32(x12,min);
-
     c0 = _mm_unpacklo_epi32(x0,x8);
     c1 = _mm_unpacklo_epi32(x4,x12);
     c2 = _mm_unpackhi_epi32(x0,x8);
@@ -359,6 +361,12 @@ void FUNC(emt_idct_VIII_4x4_h_avx2)(int16_t *x, int16_t *block, int log2_transfo
 
     c0 = _mm_packs_epi32(c0,c2);
     c1 = _mm_packs_epi32(c1,c3);
+
+    c0 = _mm_min_epi16(c0,max);
+    c0 = _mm_max_epi16(c0,min);
+
+    c1 = _mm_min_epi16(c1,max);
+    c1 = _mm_max_epi16(c1,min);
 
     x0 = _mm_unpacklo_epi16(c0,c1);
     x4 = _mm_unpackhi_epi16(c0,c1);
