720ba44aa83a54561620b357ebc57edb17b1ce5a
Pierre-Loup Cabarat <pcabarat@insa-renne.fr>
2017-07-13 12:29:46 +0200

    Don't use (avx2) opt transform_add because of a segfault in some sequences

--- libavcodec/x86/hevcdsp_init.c
+++ libavcodec/x86/hevcdsp_init.c
@@ -884,7 +884,7 @@ void ff_hevc_dsp_init_x86(HEVCDSPContext *c, const int bit_depth)
                 c->put_hevc_qpel_bi[9][1][0] = ff_hevc_put_hevc_bi_qpel_v64_8_avx2;
             }
 
-            c->transform_add[3]    = ff_hevc_transform_add32_8_avx2;
+            //c->transform_add[3]    = ff_hevc_transform_add32_8_avx2;
         }
     } else if (bit_depth == 10) {
         if (EXTERNAL_MMXEXT(cpu_flags)) {
@@ -1103,8 +1103,8 @@ void ff_hevc_dsp_init_x86(HEVCDSPContext *c, const int bit_depth)
                 c->put_hevc_qpel_bi[8][1][1] = ff_hevc_put_hevc_bi_qpel_hv48_10_avx2;
                 c->put_hevc_qpel_bi[9][1][1] = ff_hevc_put_hevc_bi_qpel_hv64_10_avx2;
             }
-            c->transform_add[2] = ff_hevc_transform_add16_10_avx2;
-            c->transform_add[3] = ff_hevc_transform_add32_10_avx2;
+            //c->transform_add[2] = ff_hevc_transform_add16_10_avx2;
+            //c->transform_add[3] = ff_hevc_transform_add32_10_avx2;
 
         }
     } else if (bit_depth == 12) {
